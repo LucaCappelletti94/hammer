9964d478bd2e1056d8105bea927f4f246a1f13fe
Luca <cappelletti.luca94@gmail.com>
2024-10-06 17:28:58 +0200

    Fixed some corner cases in visualizations

--- visualize_feature_set_synergy_performance.py
+++ visualize_feature_set_synergy_performance.py
@@ -16,18 +16,36 @@ def visualize_feature_sets_performance():
     performance = pd.read_csv("feature_sets_performance.csv")
     performance["first_feature_set"] = performance["feature_set"]
     performance["second_feature_set"] = performance["feature_set"]
-    
-    synergy_performance = pd.DataFrame(compress_json.load("feature_set_synergy_performance.json"))
 
-    performance = pd.concat([performance, synergy_performance], ignore_index=True)
+    synergy_performance = pd.DataFrame(
+        compress_json.load("feature_set_synergy_performance.json")
+    )
+
+    # We make sure that the simmetric variant are present in the dataframe, or we
+    # add them with the same value
+    symmetric_variants = []
+    for _, row in synergy_performance.iterrows():
+        symmetric_variant = row.copy()
+        symmetric_variant["first_feature_set"] = row["second_feature_set"]
+        symmetric_variant["second_feature_set"] = row["first_feature_set"]
+        symmetric_variants.append(symmetric_variant)
+
+    symmetric_variants = pd.DataFrame(symmetric_variants)
+
+    performance = pd.concat(
+        [performance, symmetric_variants, synergy_performance], ignore_index=True
+    )
 
     barplots(
         performance,
         groupby=["first_feature_set", "set", "second_feature_set"],
         orientation="horizontal",
-        height=4,
+        path="barplots/{feature}_synergy.png",
+        height=6,
+        plots_per_row=6,
         subplots=True,
-        legend_position="lower left",
+        show_last_level_as_legend=False,
+        unique_minor_labels=False,
     )
 
 

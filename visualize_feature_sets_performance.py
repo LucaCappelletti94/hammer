4b55235f734b07e126e656a94374da95e7f5f5b2
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-10-07 11:24:09 +0200

    Completed run for feature sets evaluation

--- visualize_feature_sets_performance.py
+++ visualize_feature_sets_performance.py
@@ -2,22 +2,30 @@
 
 import os
 import pandas as pd
-import compress_json
 from barplots import barplots
 
 
+def sort_bars(df: pd.DataFrame) -> pd.DataFrame:
+    """Sort the bars in the barplot."""
+
+    df_copy = df.reset_index()
+
+    df["sort_params"] = [
+        param.replace("skfp", "").lower().strip() for param in df_copy["feature_set"]
+    ]
+    df = df.sort_values(by=["sort_params"])
+    df = df.drop(columns=["sort_params"])
+
+    return df
+
 def visualize_feature_sets_performance():
     """Visualize the performance of different feature sets."""
-    # if not os.path.exists("feature_sets_performance.csv"):
-    #     raise FileNotFoundError(
-    #         "feature_sets_performance.csv not found. Run feature_set_selection.py first."
-    #     )
+    if not os.path.exists("feature_sets_performance.csv"):
+        raise FileNotFoundError(
+            "feature_sets_performance.csv not found. Run feature_set_selection.py first."
+        )
 
-    performance = pd.concat([
-        # pd.read_csv("feature_sets_performance.csv"),
-        pd.DataFrame(compress_json.load("feature_sets_performance.json"))
-    ])
-
+    performance = pd.read_csv("feature_sets_performance.csv")
     performance["feature_set"] = performance["feature_set"].str.replace("fingerprint", "")
 
     barplots(
@@ -29,6 +37,7 @@ def visualize_feature_sets_performance():
         unique_minor_labels=False,
         orientation="horizontal",
         height=6,
+        sort_bars=sort_bars,
         legend_position="lower left",
     )
 
--- visualize_features_time_requirements.py
+++ visualize_features_time_requirements.py
@@ -5,6 +5,20 @@ import pandas as pd
 from barplots import barplots
 
 
+def sort_bars(df: pd.DataFrame) -> pd.DataFrame:
+    """Sort the bars in the barplot."""
+
+    df_copy = df.reset_index()
+
+    df["sort_params"] = [
+        param.replace("skfp", "").lower().strip() for param in df_copy["parameter"]
+    ]
+    df = df.sort_values(by=["sort_params"])
+    df = df.drop(columns=["sort_params"])
+
+    return df
+
+
 def visualize_features_time_requirements():
     """Visualize the performance of different feature sets."""
     if not os.path.exists("features_time_requirements.csv"):
@@ -28,6 +42,8 @@ def visualize_features_time_requirements():
         height=6,
         show_last_level_as_legend=False,
         legend_position="upper left",
+        sort_bars=sort_bars,
+        units={"time": "s"},
     )
 
 

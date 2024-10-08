eb85b9847fbc71268afd58239e30f93b4e9b0365
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-10-08 10:46:19 +0200

    Updated visualizations for feature sets performance

--- visualize_feature_sets_performance.py
+++ visualize_feature_sets_performance.py
@@ -18,6 +18,7 @@ def sort_bars(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
+
 def visualize_feature_sets_performance():
     """Visualize the performance of different feature sets."""
     if not os.path.exists("feature_sets_performance.csv"):
@@ -26,21 +27,77 @@ def visualize_feature_sets_performance():
         )
 
     performance = pd.read_csv("feature_sets_performance.csv")
+
+    performance["feature_set"] = performance["feature_set"].str.replace(
+        "_fingerprint", ""
+    )
+
     performance_4096 = pd.read_csv("feature_sets_performance_4096.csv")
+    performance_1024 = pd.read_csv("feature_sets_performance_1024.csv")
+    performance_radius1 = pd.read_csv("feature_sets_performance_radius1.csv")
     performance_radius2 = pd.read_csv("feature_sets_performance_radius2.csv")
 
-    performance["feature_set"] = performance["feature_set"].str.replace("fingerprint", "")
-    performance_4096["feature_set"] = performance_4096["feature_set"].str.replace("fingerprint", "")
-    performance_radius2["feature_set"] = performance_radius2["feature_set"].str.replace("fingerprint", "")
+    performance_4096["feature_set"] = performance_4096["feature_set"].str.replace(
+        "_fingerprint", ""
+    )
+    performance_1024["feature_set"] = performance_1024["feature_set"].str.replace(
+        "_fingerprint", ""
+    )
+    performance_radius2["feature_set"] = performance_radius2["feature_set"].str.replace(
+        "_fingerprint", ""
+    )
+    performance_radius1["feature_set"] = performance_radius1["feature_set"].str.replace(
+        "_fingerprint", ""
+    )
+
+    original_feature_set_names = []
+    for feature in performance["feature_set"]:
+        descriptors = []
+        if feature in performance_4096["feature_set"].values:
+            descriptors.append("2048b")
+        if feature in performance_radius2["feature_set"].values:
+            descriptors.append("r3")
+        if len(descriptors) > 0:
+            original_feature_set_names.append(f"{feature} ({', '.join(descriptors)})")
+        else:
+            original_feature_set_names.append(feature)
+
+    performance["feature_set"] = original_feature_set_names
 
     performance_4096["feature_set"] = [
-        f"{feature_set} (4096 bits)" for feature_set in performance_4096["feature_set"]
+        (
+            f"{feature_set} (4096b)"
+            if feature_set not in performance_radius2["feature_set"].values
+            else f"{feature_set} (4096b, r3)"
+        )
+        for feature_set in performance_4096["feature_set"]
+    ]
+    performance_1024["feature_set"] = [
+        (
+            f"{feature_set} (1024b)"
+            if feature_set not in performance_radius2["feature_set"].values
+            else f"{feature_set} (1024b, r3)"
+        )
+        for feature_set in performance_1024["feature_set"]
     ]
     performance_radius2["feature_set"] = [
-        f"{feature_set} (radius 2)" for feature_set in performance_radius2["feature_set"]
+        f"{feature_set} (2048b, r2)"
+        for feature_set in performance_radius2["feature_set"]
+    ]
+    performance_radius1["feature_set"] = [
+        f"{feature_set} (2048b, r1)"
+        for feature_set in performance_radius1["feature_set"]
     ]
 
-    performance = pd.concat([performance, performance_4096, performance_radius2])
+    performance = pd.concat(
+        [
+            performance,
+            performance_4096,
+            performance_1024,
+            performance_radius2,
+            performance_radius1,
+        ]
+    )
 
     barplots(
         performance,
